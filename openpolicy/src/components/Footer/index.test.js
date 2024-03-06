e2870bf7ae4db75e8887ab358d93256c926d183e
Matt Edwards <mattsp.edwards@googlemail.com>
2024-03-06 21:06:36 +0000

    test: added test for Footer component

---
+++ openpolicy/src/components/Footer/index.test.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import Footer from './index';
+
+jest.mock('next/router', () => require('next-router-mock'))
+
+describe('Footer Component', () => {
+
+    test('renders without crashing', () => {
+        render(<Footer />,);
+        const footerElement = screen.getByText('OPENPOLICY');
+        expect(footerElement).toBeInTheDocument();
+    });
+
+    test('renders all menu contents', () => {
+        render(<Footer />);
+        const menuContents = [
+            "Collaborate",
+            "Review",
+            "Integrate",
+            "Support"
+        ];
+        
+        menuContents.forEach(content => {
+            expect(screen.getByText(content)).toBeInTheDocument();
+        });
+    });
+
+    test('social icons have correct aria-labels', () => {
+        render(<Footer />);
+        
+        const twitterIcon = screen.getByLabelText('Twitter');
+        const facebookIcon = screen.getByLabelText('Facebook');
+        const instagramIcon = screen.getByLabelText('Instagram');
+
+        expect(twitterIcon).toBeInTheDocument();
+        expect(facebookIcon).toBeInTheDocument();
+        expect(instagramIcon).toBeInTheDocument();
+    });
+});
