aa927ae46432db920f3935392f88d77b47ee92f2
Matt Edwards <mattsp.edwards@googlemail.com>
2024-03-25 21:39:02 +0000

    feat: added clerk provider

--- openpolicy/src/app/ConvexClientProvider.js
+++ openpolicy/src/app/ConvexClientProvider.js
@@ -1,11 +1,19 @@
 "use client";
 
-import { ConvexProvider, ConvexReactClient } from "convex/react";
+import { ClerkProvider, useAuth } from "@clerk/clerk-react";
+import { ConvexProviderWithClerk } from "convex/react-clerk";
+import { ConvexReactClient } from "convex/react";
 
 const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL);
 
 export default function ConvexClientProvider({ children }) {
-    return <ConvexProvider client={convex}>{children}</ConvexProvider>;
+    return (
+        <ClerkProvider publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}>
+            <ConvexProviderWithClerk client={convex} useAuth={useAuth}>
+                {children}
+            </ConvexProviderWithClerk>
+        </ClerkProvider>
+    )
 }
 
 
