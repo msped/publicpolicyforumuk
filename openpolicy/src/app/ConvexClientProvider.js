9cce40daa905653b24c1ec052409d797415a5f0b
Matt Edwards <mattsp.edwards@googlemail.com>
2024-03-25 23:50:28 +0000

    feat: gave clerk components a baseTheme of dark

--- openpolicy/src/app/ConvexClientProvider.js
+++ openpolicy/src/app/ConvexClientProvider.js
@@ -3,12 +3,18 @@
 import { ClerkProvider, useAuth } from "@clerk/clerk-react";
 import { ConvexProviderWithClerk } from "convex/react-clerk";
 import { ConvexReactClient } from "convex/react";
+import { dark } from '@clerk/themes';
 
 const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL);
 
 export default function ConvexClientProvider({ children }) {
     return (
-        <ClerkProvider publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}>
+        <ClerkProvider 
+            publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}
+            appearance={{
+                baseTheme: dark
+            }}
+        >
             <ConvexProviderWithClerk client={convex} useAuth={useAuth}>
                 {children}
             </ConvexProviderWithClerk>
