eadb0d07f2ec27dd32f49269939e6be455ccb3a7
Matt Edwards <mattsp.edwards@googlemail.com>
2024-03-30 14:28:46 +0000

    build: added clerk signIn and signOut commands for cypress

--- openpolicy/cypress/support/commands.js
+++ openpolicy/cypress/support/commands.js
@@ -9,8 +9,36 @@
 // ***********************************************
 //
 //
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
+
+Cypress.Commands.add(`signIn`, () => {
+    cy.log(`Signing in.`);
+    cy.visit(`/`);
+
+    cy.window()
+        .should((window) => {
+            expect(window).to.not.have.property(`Clerk`, undefined);
+            expect(window.Clerk.isReady()).to.eq(true);
+        })
+        .then(async (window) => {
+            await cy.clearCookies({ domain: window.location.domain });
+            const res = await window.Clerk.client.signIn.create({
+            identifier: Cypress.env(`test_email`),
+            password: Cypress.env(`test_password`),
+        });
+
+        await window.Clerk.setActive({
+            session: res.createdSessionId,
+        });
+
+        cy.log(`Finished Signing in.`);
+    });
+});
+
+Cypress.Commands.add(`signOut`, () => {
+    cy.log(`sign out by clearing all cookies.`);
+    cy.clearCookies({ domain: null });
+});
+
 //
 //
 // -- This is a child command --
