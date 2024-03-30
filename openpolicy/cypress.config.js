c64a5fa669788349214407387e481d6aca7a04f5
Matt Edwards <mattsp.edwards@googlemail.com>
2024-03-30 22:42:12 +0000

    build: added BaseUrl to cypresss config

--- openpolicy/cypress.config.js
+++ openpolicy/cypress.config.js
@@ -7,4 +7,11 @@ module.exports = defineConfig({
       bundler: "webpack",
     },
   },
+
+  e2e: {
+    baseUrl: "http://localhost:3000",
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
 });
