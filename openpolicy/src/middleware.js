524aab63919676193454f4310bafca62d69873b8
Matt Edwards <mattsp.edwards@googlemail.com>
2024-04-03 21:54:35 +0100

    feat: added public links to publicRoutes in the middleware

--- openpolicy/src/middleware.js
+++ openpolicy/src/middleware.js
@@ -1,7 +1,7 @@
 import { authMiddleware } from "@clerk/nextjs";
 
 export default authMiddleware({
-    publicRoutes: ["/"],
+    publicRoutes: ["/", '/design', '/collaborate', '/integrate', '/support'],
     ignoredRoutes: ["/favicon.ico", "/logo.webp"]
 })
 
